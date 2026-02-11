159afbad982acc81b690a27619041791f30dce55
Nathan Partouche <nathanpartouche2@gmail.com>
2026-02-12 00:37:24 +0200

    add input on comment + id_user and post when you comment

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,15 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class CommentCrudController extends AbstractCrudController
 {
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance instanceof \App\Entity\Comment) {
+            return;
+        }
+
+        $entityInstance->setUser($this->getUser());
+        $entityInstance->setCreatedAt(new \DateTimeImmutable());
+        $entityInstance->setUpdatedAt(new \DateTimeImmutable());
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
     public function configureFilters(Filters $filters): Filters
     {
         return $filters
@@ -24,14 +40,18 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextEditorField::new('contenu'),
+            AssociationField::new('user', 'Auteur')
+                ->formatValue(function ($value) {
+                    return $value ? $value->getPrenom() . ' ' . $value->getNom() : '';
+                })
+                ->onlyOnIndex(),
+            AssociationField::new('post')
+                ->setRequired(true),
         ];
     }
-    */
 }
